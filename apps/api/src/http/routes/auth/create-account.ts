8c398869ee42659c9354f841c5da93f42e80bfe1
Wander Amaral <wanderamaral013@gmail.com>
2024-05-24 18:46:50 -0300

    commit

---
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -0,0 +1,46 @@
+import { hash } from 'bcryptjs'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { prisma } from 'src/lib/prisma'
+import { z } from 'zod'
+
+export async function createAccount(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/users',
+    {
+      schema: {
+        summary: 'Create new account',
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+          password: z.string().min(6),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { name, email, password } = request.body
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: { email },
+      })
+
+      if (userWithSameEmail) {
+        return reply
+          .status(400)
+          .send({ message: 'user with same e-mail already exists.' })
+      }
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      })
+
+      return reply.status(201).send()
+    },
+  )
+}
